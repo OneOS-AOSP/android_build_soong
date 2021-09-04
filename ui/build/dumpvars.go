5663c1915a8178adca1a5cf29bf4b25ad4b19e93
BadWolf-Alfa <marco@lunawolf.es>
2021-09-04 11:32:13 +0200

    ONEfy

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -137,7 +137,7 @@ func dumpMakeVars(ctx Context, config Config, goals, vars []string, write_soong_
 
 // Variables to print out in the top banner
 var BannerVars = []string{
-	"NAD_VERSION",
+	"ONE_VERSION",
 	"TARGET_PRODUCT",
 	"TARGET_BUILD_VARIANT",
 	"PRODUCT_SOONG_NAMESPACES",
